488eb24616d9249c0cccebf726a89f021ec9a48c
Pratik R Raval <111408819+rpratik15@users.noreply.github.com>
2022-12-12 22:33:46 +0530

    update1

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+fetch("https://randomuser.me/api")
+.then((res)=>{
+    return res.json()
+})
+.then((user)=>{
+    // console.log( user.results)
+    displayData(user.results[0])
+
+})
+.catch((error)=>console.log(error))
+
+function displayData(data)
+{
+    console.log(data.picture.large)
+    document.getElementById('fullname').innerHTML=`${data.name.title} ${data.name.first} ${data.name.last}`;
+    document.getElementById('userimg').src=data.picture.large;
+}
+// axios.get('https://randomuser.me/api/')
+// .then((res)=>{
+//     document.getElementById('fullname').innerHTML= res.data
+//     })
+// .catch(err=>console.log(err))
+
+// console.log(result)
